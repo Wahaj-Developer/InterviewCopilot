06dabf246f6d937fa76f6d239bca43b485c218e7
Wahaj Khan Ahmed <ahmedwahaj317@gmail.com>
2026-07-20 02:15:10 +0500

    Add interview report retrieval and PDF generation controllers

--- Backend/src/controllers/interview.controller.js
+++ Backend/src/controllers/interview.controller.js
@@ -58,3 +58,81 @@ async function generateInterviewReportController (req,res) {
         })
     }
 }
+
+
+/**
+ *  @description controller to get interview report  by its ID
+ */
+
+async function getInterviewByIdController(req, res) {
+  try {
+    const { interviewId } = req.params;
+
+    const interviewReport = await interviewReportModel.findOne({ _id: interviewId, user: req.user._id });
+    if (!interviewReport) {
+      return res.status(404).json({ message: "Interview report not found" });
+    }
+
+    res.status(200).json({ 
+      message: "Interview report retrieved successfully",
+      interviewReport: interviewReport
+    });
+  } catch(error) {
+    console.error('Error retrieving interview report:', error);
+    res.status(500).json({
+      message: "Error retrieving interview report",
+      error: error.message
+    });
+  }
+}
+
+
+/**
+ * @description controller to get all interview reports of a loged in user
+ */
+
+async function getAllInterviewsController(req, res) {
+  try {
+    const interviewReports = await interviewReportModel.find({ user: req.user._id }).sort({ createdAt: -1 }).select('-resume -selfDescription -jobDescription -__v -technicalQuestion -behaviorQuestion -skillGaps -preparationPlan'); 
+
+    res.status(200).json({
+      message: "Interview reports fetched successfully",
+      interviewReports: interviewReports
+    });
+  } catch(error) {
+    console.error('Error fetching interview reports:', error);
+    res.status(500).json({
+      message: "Error fetching interview reports",
+      error: error.message
+    });
+  }
+}
+
+/**
+ * @description controller to generate resume pdf from user description,job description and resume content
+ */
+
+async function generateResumePdfController(req, res) {
+ const {interviewReportId} = req.params
+ const interviewReport = await interviewReportModel.findById(interviewReportId)
+
+ if(!interviewReport){
+  return res.status(404).json({
+    message:"Interview Report not found"
+  })
+ }
+ const {resume,jobDescription,selfDescription} = interviewReport
+ const pdfBuffer = await generateResumePdf({resume,jobDescription,selfDescription})
+ res.set({
+  "Content-Type":"application/pdf",
+  "Content-Desposition": `attachment; filename=resume_${interviewReportId}.pdf`
+ })
+ res.send(pdfBuffer)
+}
+
+module.exports = {
+     generateInterviewReportController,
+     getInterviewByIdController,
+      getAllInterviewsController,
+      generateResumePdfController
+}
