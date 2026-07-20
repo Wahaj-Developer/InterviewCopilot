4f4afd079fb74456b05c1794c4d779acb8719ec7
Wahaj Khan Ahmed <ahmedwahaj317@gmail.com>
2026-07-21 00:39:27 +0500

    Update auth.middleware.js

--- Backend/src/middlewares/auth.middleware.js
+++ Backend/src/middlewares/auth.middleware.js
@@ -1,1 +1,26 @@
+const jwt = require('jsonwebtoken');
+const tokenBlacklistModel = require('../config/models/blacklist.model');
 
+
+async function authUser(req, res, next) {
+    const token = req.cookies.token;
+    if (!token) {
+        return res.status(401).json({ message: 'Unauthorized, token is missing' });
+    }
+ const isTokenBlacklisted = await tokenBlacklistModel.findOne({ token });
+ if (isTokenBlacklisted) {
+return res.status(401).json({ message: 'Unauthorized, token is blacklisted' });
+ }
+
+try{
+  const decoded =  jwt.verify(token, process.env.JWT_SECRET)
+    req.user = decoded;
+    next();
+
+}catch(error){
+    return res.status(401).json({message:'Unauthorized, invalid token'})
+}
+
+
+}
+module.exports = {authUser};
